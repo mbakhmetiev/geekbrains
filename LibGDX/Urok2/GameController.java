1b7a9e4ab1dfe5f61e0e0fdeaa778f704e986808
mbakhmetiev <mbakhmetiev@yandex.ru>
2019-12-03 23:00:44 +0300

    Update GameController.java

--- LibGDX/Urok2/GameController.java
+++ LibGDX/Urok2/GameController.java
@@ -1,26 +1,53 @@
 package com.mygdx.game.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+public class GameController {
+    private Background background;
+    private BulletController bulletController;
+    private AsteroidController asteroidController;
+    private Hero hero;
+    private AsteroidSpawn asteroidSpawn;
 
-public class WorldRenderer {
-    private GameController gc;
-    private SpriteBatch batch;
-
-    public WorldRenderer(GameController gc, SpriteBatch batch) {
-        this.gc = gc;
-        this.batch = batch;
+    public Background getBackground() {
+        return background;
     }
 
-    public void render() {
-        Gdx.gl.glClearColor(0.4f, 0.2f, 0.6f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        gc.getBackground().render(batch);
-        gc.getHero().render(batch);
-        gc.getBulletController().render(batch);
-        gc.getAsteroidController().render(batch);
-        batch.end();
+    public Hero getHero() {
+        return hero;
+    }
+
+    public BulletController getBulletController() {
+        return bulletController;
+    }
+
+    public AsteroidController getAsteroidController() {
+        return asteroidController;
+    }
+
+    public GameController() {
+        this.background = new Background(this);
+        this.hero = new Hero(this);
+        this.bulletController = new BulletController();
+        this.asteroidController = new AsteroidController();
+        this.asteroidSpawn = new AsteroidSpawn(this);
+    }
+
+    public void update(float dt) {
+        background.update(dt);
+        hero.update(dt);
+        bulletController.update(dt);
+        asteroidSpawn.update(dt);
+        asteroidController.update(dt);
+        checkCollisions();
+    }
+
+    public void checkCollisions () {
+        for (Bullet b : bulletController.getActiveList()) {
+            for (Asteroid a : asteroidController.getActiveList()) {
+                if (b.getPosition().dst(a.getPosition()) < (120.0f * a.getScale())) {
+                    a.deactivate(); b.deactivate();
+                }
+            }
+        }
     }
 }
+
